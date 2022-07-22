de33f9707a7fcc7e38592ef9aecefd89afb73bee
yesahek <yesahek@gmail.com>
2022-07-22 23:29:23 +0300

    categorys_screen

---
+++ mealapp/lib/categories_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:mealapp/categroy_item.dart';
+import 'package:mealapp/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      children: DUMMY_CATEGORIES
+          .map((catData) => CategroyItem(catData.title, catData.color))
+          .toList(),
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+    );
+  }
+}
